85a84916eb315cb557b264756dade905822d0eba
Your Name <marvin.delansorne@laplateforme.io>
2025-01-13 14:26:32 +0100

    Job 01

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$str = " la Plateforme";
+
+$str2 = "Vive" ; 
+$str3 = "!" ;
+
+echo $str2, $str, $str3  ;
+
+$val = 6 ;
+
+echo $val ;
+
+echo $val + (4) ; 
+
+$myBool = "true";
+
+echo $myBool ;
+
+$myBool = "false";
+
+echo $myBool ; 
