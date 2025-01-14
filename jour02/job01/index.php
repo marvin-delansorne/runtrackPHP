b98bcf6a18188439b94e7666f58c4dbad1723ebe
Your Name <marvin.delansorne@laplateforme.io>
2025-01-14 10:23:45 +0100

    Job 01 Jour 2 (Boucle)

---
+++ jour02/job01/index.php
@@ -0,0 +1,25 @@
+<?php
+ 
+ $nombre = 0;
+
+ while ($nombre <= 1337 ){
+    
+    if ($nombre == 42){
+        echo "<strong><u>$nombre</u></strong><br>";
+    }
+
+    else {
+        echo  $nombre , '<br>' ;
+    }
+
+    
+    
+    $nombre++;
+
+ }
+ 
+
+
+
+
+?>
