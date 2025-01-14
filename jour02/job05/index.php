002ad6bbfe1b177fdaa306a63844cba97bb6c768
Your Name <marvin.delansorne@laplateforme.io>
2025-01-14 13:49:56 +0100

    ?Nombre premier job 05 Jour 02

---
+++ jour02/job05/index.php
@@ -0,0 +1,18 @@
+<?php
+
+for ($i = 2; $i <= 1000; $i++) {
+    $estPremier = true;
+    for ($j = 2; $j < $i; $j++) {
+        if ($i % $j == 0) {
+            $estPremier = false;
+            break;
+        }
+    }
+    if ($estPremier) {
+        echo $i . "<br>";
+    }
+}
+
+
+  
+ 
