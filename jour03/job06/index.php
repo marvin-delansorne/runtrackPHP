98744df1ee42b74d6e7a7cd3e16e4f5c9aab95d5
Your Name <marvin.delansorne@laplateforme.io>
2025-01-15 14:09:30 +0100

    Job 06 jour 03 lire une chaine de caractère d'une liste à l'enver

---
+++ jour03/job06/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$str = "Les choses que l on Possede finissent par nous posseder.";
+
+$reversedStr = "";
+
+for ($i = strlen($str) - 1; $i >= 0; $i--) {
+    $reversedStr .= $str[$i];
+}
+
+
+echo $reversedStr;
+?>
