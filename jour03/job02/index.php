ea5e0ae05df9a10ff100ccc3b486a71c3aa524d6
Your Name <marvin.delansorne@laplateforme.io>
2025-01-15 14:56:33 +0100

    Job 02 Jour 03 sans fonction système

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,9 +1,11 @@
 <?php
 
-$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie" ;
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie";
 
-$longueur = strlen($str) ;
-
-for ($i = 0 ; $i < $longueur; $i += 2) { 
-    echo $str[$i] ;
+$i = 0;
+while (isset($str[$i])) {
+    if ($i % 2 == 0) {
+        echo $str[$i];
+    }
+    $i++;
 }
