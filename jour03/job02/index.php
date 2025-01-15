dd8dc1c6b16a53b29d8287127d5180f8d4d641b3
Your Name <marvin.delansorne@laplateforme.io>
2025-01-15 11:25:20 +0100

    Job 02 Jour 03 Parcourir les chaine de caractère avec Strlen

---
+++ jour03/job02/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie" ;
+
+$longueur = strlen($str) ;
+
+for ($i = 0 ; $i < $longueur; $i += 2) { 
+    echo $str[$i] ;
+}
+
+echo $str[5];
